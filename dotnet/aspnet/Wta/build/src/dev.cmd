e760190038c616c342ea0cf5b50a98cfac080544
wanggang <76527413@qq.com>
2024-09-23 16:51:41 +0800

    update

--- dotnet/aspnet/Wta/build/src/dev.cmd
+++ dotnet/aspnet/Wta/build/src/dev.cmd
@@ -2,3 +2,4 @@ docker compose -f docker-compose.base.yml up -d
 docker compose -f docker-compose.minio.yml up -d
 docker compose -f docker-compose.redis.yml up -d
 docker compose -f docker-compose.mysql.yml up -d
+docker compose -f docker-compose.yml -f docker-compose.development.yml up -d
