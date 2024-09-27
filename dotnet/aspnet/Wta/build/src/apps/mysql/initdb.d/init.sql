882c5e0e85af09b226f1a39ff4c08075c1dcf605
wanggang <76527413@qq.com>
2024-09-27 16:55:25 +0800

    update

--- dotnet/aspnet/Wta/build/src/apps/mysql/initdb.d/init.sql
+++ dotnet/aspnet/Wta/build/src/apps/mysql/initdb.d/init.sql
@@ -5,3 +5,5 @@ CREATE USER 'replicator'@'%' IDENTIFIED WITH mysql_native_password BY 'password'
 GRANT SELECT, RELOAD, SHOW DATABASES, REPLICATION SLAVE, REPLICATION CLIENT ON *.* TO 'replicator'@'%';
 
 FLUSH PRIVILEGES;
+
+-- CREATE DATABASE dinky;
