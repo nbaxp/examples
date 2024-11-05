e6150826bc10dfc64ef92cc5a01fb62b04b3b0c0
wanggang <76527413@qq.com>
2024-11-05 16:31:01 +0800

    update

--- dotnet/aspnet/Wta/be/src/Wta.Application/Tpm/Domain/CheckPlan.cs
+++ dotnet/aspnet/Wta/be/src/Wta.Application/Tpm/Domain/CheckPlan.cs
@@ -1,23 +1,62 @@
+using System.ComponentModel.DataAnnotations.Schema;
 using Wta.Application.BaseData.Domain;
 using Wta.Application.Platform;
 
 namespace Wta.Application.Tpm.Domain;
 
-[Display(Name = "点检计划",Order =30)]
+[Display(Name = "点检计划", Order = 30)]
 [DependsOn<PlatformDbContext>]
 [DevicePlanCheck]
 public class CheckPlan : BaseNameNumberEntity
 {
     public List<PlanProject> PlanProjects { get; set; } = [];
+
+    [KeyValue("hideForList", true)]
+    [UIHint("select")]
+    [KeyValue("url", "check-project/search")]
+    [KeyValue("value", "id")]
+    [KeyValue("label", "name")]
+    [NotMapped]
+    [Display(Name = "项目")]
+    public List<Guid> Projects
+    {
+        get
+        {
+            return this.PlanProjects?.Select(o => o.ProjectId).ToList() ?? [];
+        }
+        set
+        {
+            this.PlanProjects = value?.Distinct().Select(o => new PlanProject { ProjectId = o }).ToList() ?? [];
+        }
+    }
 }
 
-[Display(Name = "点检项目",Order =20)]
+[Display(Name = "点检项目", Order = 20)]
 [DependsOn<PlatformDbContext>]
 [DevicePlanCheck]
 public class CheckProject : BaseNameNumberEntity
 {
     public List<PlanProject> PlanProjects { get; set; } = [];
     public List<ProjectStandard> PorjectStandards { get; set; } = [];
+
+    [KeyValue("hideForList", true)]
+    [UIHint("select")]
+    [KeyValue("url", "check-standard/search")]
+    [KeyValue("value", "id")]
+    [KeyValue("label", "name")]
+    [NotMapped]
+    [Display(Name = "标准")]
+    public List<Guid> Standards
+    {
+        get
+        {
+            return this.PorjectStandards?.Select(o => o.StandardId).ToList() ?? [];
+        }
+        set
+        {
+            this.PorjectStandards = value?.Distinct().Select(o => new ProjectStandard { StandardId = o }).ToList() ?? [];
+        }
+    }
 }
 
 [DependsOn<PlatformDbContext>]
@@ -40,7 +79,7 @@ public class PlanProject : ISoftDelete, ITenant, IEntityTypeConfiguration<PlanPr
     }
 }
 
-[Display(Name = "点检标准",Order =10)]
+[Display(Name = "点检标准", Order = 10)]
 [DependsOn<PlatformDbContext>]
 [DevicePlanCheck]
 public class CheckStandard : BaseNameNumberEntity, IEntityTypeConfiguration<CheckStandard>
