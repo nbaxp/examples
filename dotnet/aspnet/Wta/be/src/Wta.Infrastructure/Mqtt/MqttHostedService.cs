dba31dd42e3e988433e439cad8d828323070cf16
wanggang <76527413@qq.com>
2024-11-07 16:19:59 +0800

    update

--- dotnet/aspnet/Wta/be/src/Wta.Infrastructure/Mqtt/MqttHostedService.cs
+++ dotnet/aspnet/Wta/be/src/Wta.Infrastructure/Mqtt/MqttHostedService.cs
@@ -1,4 +1,6 @@
 using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Extensions.ManagedClient;
 using MQTTnet.Protocol;
 
 namespace Wta.Infrastructure.Mqtt;
@@ -6,63 +8,55 @@ namespace Wta.Infrastructure.Mqtt;
 [Service<IHostedService>]
 public class MqttHostedService(IConfiguration configuration) : IHostedService
 {
-    public IMqttClient Client { get; private set; } = default!;
+    public IManagedMqttClient Client { get; private set; } = default!;
 
     public async Task StartAsync(CancellationToken cancellationToken)
     {
         var connectionString = configuration.GetConnectionString("Mqtt")!;
         var builder = new UriBuilder(connectionString);
-        var mqttClientOptions = new MqttClientOptionsBuilder()
-            .WithClientId("wta.platform")
-            .WithTcpServer(builder.Host, builder.Port)
-            .WithCredentials(builder.UserName, Encoding.UTF8.GetBytes(builder.Password))
-            .WithCleanSession(false)
-            .WithSessionExpiryInterval(uint.MaxValue)
+        var options = new ManagedMqttClientOptionsBuilder()
+            .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+            .WithClientOptions(new MqttClientOptionsBuilder()
+                .WithClientId("wta.platform")
+                .WithTcpServer(builder.Host, builder.Port)
+                .WithCredentials(builder.UserName, Encoding.UTF8.GetBytes(builder.Password))
+                .WithCleanSession(false)
+                .WithWillQualityOfServiceLevel(MqttQualityOfServiceLevel.ExactlyOnce)
+                .WithSessionExpiryInterval(uint.MaxValue))
             .Build();
-        var mqttFactory = new MqttClientFactory();
-        Client = mqttFactory.CreateMqttClient();
+        Client = new MqttFactory().CreateManagedMqttClient();
         Client.ApplicationMessageReceivedAsync += e =>
         {
-            Console.WriteLine("Received application message.");
-            var message = e.ApplicationMessage.ConvertPayloadToString();
-            //e.ReasonCode = MqttApplicationMessageReceivedReasonCode.Success;
-            //e.ResponseReasonString = e.ReasonCode.ToString();
-            e.IsHandled = true;
-            Console.WriteLine(message);
-            return Task.CompletedTask;
-        };
-        await Connect(mqttClientOptions).ConfigureAwait(false);
-        _ = Task.Run(async () =>
-        {
-            while (!cancellationToken.IsCancellationRequested)
+            try
             {
-                try
+                Console.WriteLine("Received application message.");
+                var message = e.ApplicationMessage.ConvertPayloadToString();
+                Console.WriteLine(message);
+                //e.ReasonCode = MqttApplicationMessageReceivedReasonCode.Success;
+                //e.ResponseReasonString = e.ReasonCode.ToString();
+                //e.IsHandled = true;
+                var dict = JsonSerializer.Deserialize<Dictionary<string, object>>(message)!;
+                if (int.TryParse(dict["msg"].ToString(), out var result) && result % 2 == 0)
                 {
-                    if (!await Client.TryPingAsync().ConfigureAwait(false))
-                    {
-                        await Connect(mqttClientOptions).ConfigureAwait(false);
-                    }
-                    else
-                    {
-                        Console.WriteLine("MQTT 客户端连接可用");
-                    }
+                    throw new Exception("message need resent");
                 }
-                catch (Exception ex)
-                {
-                    Console.WriteLine("MQTT 客户端连接失败");
-                    Console.WriteLine(ex.Message);
-                    Console.WriteLine(ex.ToString());
-                }
-                await Task.Delay(5 * 1000).ConfigureAwait(false);
+                return Task.CompletedTask;
             }
-        }, cancellationToken).ConfigureAwait(false);
-        await Task.CompletedTask.ConfigureAwait(false);
+            catch (Exception ex)
+            {
+                e.ProcessingFailed = true;
+                return Task.FromException(ex);
+            }
+        };
+        await Client.SubscribeAsync("topic/#", MqttQualityOfServiceLevel.ExactlyOnce).ConfigureAwait(false);
+        await Client.StartAsync(options).ConfigureAwait(false);
     }
 
     public Task StopAsync(CancellationToken cancellationToken)
     {
         try
         {
+            Client?.StopAsync();
             Client?.Dispose();
         }
         catch (Exception ex)
@@ -72,21 +66,5 @@ public class MqttHostedService(IConfiguration configuration) : IHostedService
             Console.WriteLine(ex.ToString());
         }
         return Task.CompletedTask;
-    }
-
-    private async Task Connect(MqttClientOptions mqttClientOptions)
-    {
-        try
-        {
-            await Client.ConnectAsync(mqttClientOptions, CancellationToken.None).ConfigureAwait(false);
-            await Client.SubscribeAsync("topic/#", MqttQualityOfServiceLevel.ExactlyOnce).ConfigureAwait(false);
-            Console.WriteLine("MQTT 客户端连接成功");
-        }
-        catch (Exception ex)
-        {
-            Console.WriteLine("MQTT 客户端连接失败");
-            Console.WriteLine(ex.Message);
-            Console.WriteLine(ex.ToString());
-        }
     }
 }
